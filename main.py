ffc510592ad1f0260a2b6531a098c629171cfce1
mrzrow <mrzrow@yandex.ru>
2025-05-04 17:36:07 +0300

    added main file

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+from api import Api
+from graph import Graph
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    token = os.environ.get('ACCESS-TOKEN')
+
+    vk = Api(token=token)
+    gr = Graph()
+    
+    try:
+        user_id = int(input('Введите свой ID пользователя: '))
+    except ValueError:
+        raise ValueError('ID пользователя должен являться числом')
+    
+    attribute = input('Введите признак, по которому будет строится граф: ')
+    
+    friends = vk.get_friends(user_id=user_id)
+    gr.plot_graph(friends=friends, attribute=attribute)
+
